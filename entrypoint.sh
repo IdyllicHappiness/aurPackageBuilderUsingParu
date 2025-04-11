916d59b2567febc93a80082dbf39314acc726b09
IdyllicHappiness <capavanni@outlook.com>
2025-04-11 04:37:52 +0100

    Initial commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #! /usr/bin/env bash
 
+set -ex
+
 echo "Would build ${INPUT_PACKAGE}"
 
+paru -G "$INPUT_PACKAGE"
 
+cd "$INPUT_PACKAGE" || exit 1
+
+tempOutputDir=$(mktemp -d)
+
+PKGDEST="$tempOutputDir" paru -B --noconfirm .
+
+cp -a "$tempOutputDir"/* /github/workspace
